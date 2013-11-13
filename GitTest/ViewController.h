a67e011f342918892fddb6c44afb70e0546bb449
John <aisini7598@qq.com>
2013-11-13 14:23:45 +0800

    afa

---
+++ GitTest/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  GitTest
+//
+//  Created by John on 13-11-13.
+//  Copyright (c) 2013年 John. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ GitTest/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  GitTest
+//
+//  Created by John on 13-11-13.
+//  Copyright (c) 2013年 John. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
